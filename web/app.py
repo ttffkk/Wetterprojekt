f51a4907cfab97f2ab9bd91ec3f0129ae7a4d12a
TIM <tfkaminski@gmail.com>
2025-11-06 16:06:46 +0100

    refactor: Reorganize config.yaml and update parsing logic
    This commit reorganizes the `config.yaml` file to group related `source` variables into nested structures, improving readability and maintainability. The parsing logic in `web/app.py` has been updated to accommodate the new nested configuration structure.
    
    - **Config Reorganization:** The `source` section in `config.yaml` is now organized into `patterns`, `file_properties`, and `paths` subsections.
    - **Code Update:** The `import_data` function in `web/app.py` has been updated to correctly parse the new nested configuration.

--- web/app.py
+++ web/app.py
@@ -29,20 +29,23 @@ def import_data():
         config = yaml.safe_load(file)
 
     source_config = config['source']
+    patterns_config = source_config['patterns']
+    file_properties_config = source_config['file_properties']
+    paths_config = source_config['paths']
     db_config = config['database']
 
     # 0. Create the database and tables
-    db = Database(db_config['path'], source_config['na_value'], source_config['file_encoding'])
+    db = Database(db_config['path'], file_properties_config['na_value'], file_properties_config['file_encoding'])
     db.create_connection()
     db.create_tables(db_config['sql_file_path'])
 
     # 1. Instantiate the classes
-    downloader = Downloader(url=source_config['url'], download_dir=source_config['download_dir'])
-    processor = DataProcessor(source_config['download_dir'], source_config['extract_dir'], source_config['file_encoding'], source_config['na_value'])
+    downloader = Downloader(url=source_config['url'], download_dir=paths_config['download_dir'])
+    processor = DataProcessor(paths_config['download_dir'], paths_config['extract_dir'], file_properties_config['file_encoding'], file_properties_config['na_value'])
     importer = CsvImporter(db)
 
     # 2. Get all file URLs
-    file_urls = downloader.get_file_urls(pattern=source_config['zip_pattern'])
+    file_urls = downloader.get_file_urls(pattern=patterns_config['zip_pattern'])
 
     # 3. Process each file one by one
     for url in file_urls:
@@ -50,12 +53,12 @@ def import_data():
         if zip_file_path:
             csv_file_path = processor.process_file(
                 zip_file_path,
-                source_config['product_pattern_to_extract'],
-                source_config['header_keyword'],
-                source_config['delimiter']
+                patterns_config['product_pattern_to_extract'],
+                patterns_config['header_keyword'],
+                file_properties_config['delimiter']
             )
             if csv_file_path:
-                importer.import_file(csv_file_path, source_config['delimiter'])
+                importer.import_file(csv_file_path, file_properties_config['delimiter'])
                 os.remove(csv_file_path)
             os.remove(zip_file_path)
 
